b3ebf32558d58d27da651a114c17be4a912e1757
lu_maraes <50714793+Lumaraescoder@users.noreply.github.com>
2021-12-05 11:09:09 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+function doGet() {
+  var sheet = SpreadsheetApp.getActiveSheet();
+  var data = sheet.getDataRange().getValues();
+   const jsonData = convertToJson(data);
+      for (var i = 0; i < data.length; i++) {
+    // Logger.log('Product name: ' + data[i][0]);
+    // Logger.log('Product number: ' + data[i][1]);
+    console.log(data)
+  }
+  return ContentService
+    .createTextOutput(JSON.stringify(jsonData))
+    .setMimeType(ContentService.MimeType.JSON);
+}
+
+function convertToJson(data) {
+  const headers = data[0]
+  const raw_data = data.slice(1,)
+  let json = []
+  raw_data.forEach(d => {
+      let object = {}
+      for (let i = 0; i < headers.length; i++) {
+        object[headers[i]] = d[i]
+      }
+      json.push(object)
+  });
+  return json
+}
+
+
+
